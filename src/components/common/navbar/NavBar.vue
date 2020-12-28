4534de0eb54423f0577afca0cefcc2219817db9d
q-div <1716193070@qq.com>
2020-12-28 14:00:22 +0800

    'chushihuaxiangmu'

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="nav-bar">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}
+</script>
+
+<style scoped>
+  .nav-bar {
+      width: 100%;
+      display: flex;
+      height: 64px;
+      line-height: 64px;
+  }
+
+  .left {
+      width: 250px;
+      height: 64px;
+      /* background-color: red; */
+  }
+
+  .right {
+      flex: 1;
+      /* background-color: pink; */
+  }
+</style>
